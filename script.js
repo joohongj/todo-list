8d30ff8d10887b33c8f26d0016ea5301a2e7f88e
Joohong Jung <joohongj@gmail.com>
2026-02-26 02:04:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ADD YOUR CODE BELOW 
 
-// 1. Start with an array of strings (ex: "grapes", "bread", "tea")
+// 1. Start with an array of strings
 let todoItems = [
     "pho",
     "sushi",
@@ -14,36 +14,24 @@ let todoItems = [
     "wine",
     "beer",
     "hojicha"
-
     // add more items here
 ];
 
 
-
 // 2. Create variables for each interactive DOM element
 const addItemButton = document.getElementById('add-item-button');
-// add more variables below
+const itemInput = document.getElementById('text');
 const list = document.getElementById('list');
-const sortItemButton = document.getElementById('sort');
-const clearAllButton = document.getElementById('clear');
-
+const sortBtn = document.getElementById('sort');
+const clearBtn = document.getElementById('clear');
 
 
 // 3. Write a function to display all items in the #list element
 function updateList() {
-    // add your code here
-       list.innerHTML = "";
-
-     todoItems.forEach((item, index) => {
+    list.innerHTML = "";
+    todoItems.forEach(function (item) {
         const li = document.createElement("li");
         li.textContent = item;
-
-        // Optional: remove item when clicked
-        li.addEventListener("click", () => {
-            todoItems.splice(index, 1);
-            updateList();
-        });
-
         list.appendChild(li);
     });
 }
@@ -51,29 +39,29 @@ function updateList() {
 updateList();
 
 
-
-// 4. Handle adding a new item when the form is submitted
+// 4. Handle adding a new item when the button is clicked
 addItemButton.addEventListener('click', function (event) {
+    event.preventDefault();
 
-    // add your code here
+    const newItem = itemInput.value.trim();
+
+    if (newItem !== "") {
+        todoItems.push(newItem);
+        itemInput.value = "";
+        updateList();
+    }
 });
-
-
 
 
 // 5. Sort items alphabetically when sortBtn is clicked
 sortBtn.addEventListener("click", () => {
-    // add your code here
-    const name_AZ = (a, b) =>(a.name > b.name ? 1 : b.name > a.name ? -1 : 0);
-    
-
+    todoItems.sort();
+    updateList();
 });
-
-
 
 
 // 6. Clear all items when clearBtn is clicked
 clearBtn.addEventListener("click", () => {
-    // add your code here
+    todoItems = [];
+    updateList();
 });
-
